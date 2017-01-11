67f9a6ab85b2ba2231fb8729f08e5e2e5dce1b49
Valdemyar <valdemyar@styleru.net>
2017-01-11 16:33:29 +0300

    Initial Commit

---
+++ sixHandsTests/sixHandsTests.swift
@@ -0,0 +1,36 @@
+//
+//  sixHandsTests.swift
+//  sixHandsTests
+//
+//  Created by Владимир Марков on 11.01.17.
+//  Copyright © 2017 Владимир Марков. All rights reserved.
+//
+
+import XCTest
+@testable import sixHands
+
+class sixHandsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

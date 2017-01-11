79d1081a7da6e89cabba270fda9479d1ef8c841d
Valdemyar <valdemyar@styleru.net>
2017-01-11 16:39:02 +0300

    Pods

---
+++ Pods/Target Support Files/Pods-sixHandsUITests/Pods-sixHandsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sixHandsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_sixHandsUITests
+@end
---
+++ Pods/Target Support Files/Pods-sixHandsUITests/Pods-sixHandsUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_sixHandsUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_sixHandsUITestsVersionString[];
+
